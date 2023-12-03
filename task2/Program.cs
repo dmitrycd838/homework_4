8658ee9abe9bd694f8d2a6f160ef872aa8064329
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-12-03 13:21:32 +0300

    Добавил разные способы решения задачи

--- task2/Program.cs
+++ task2/Program.cs
@@ -24,18 +24,49 @@ void PrintArray(int[] array)
     System.Console.WriteLine("[" + string.Join(", ", array) + "]");
 }
 
+int CountDivBy7(int[] arr)
+{
+    int count = 0;
+    for (int number = 0; number < arr.Length; number++)
+    {
+        if (arr[number] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count; 
+}
+
 // Объявим массив, который равен работе метода GenerateArray, внутрь метода мы отправим
 // 1. размер генерируемого массива
 // 2. левую границу генерации.
 // 3. правую границу генерации. 
-int[] myArray = GenerateArray(10, 100, 1000); 
+int[] myArray = GenerateArray(10, 100, 1000);
 PrintArray(myArray);
 
-int count = 0;
+// int count = 0;
 
-foreach ( int number in myArray)
-    if (number % 2 == 0)
-    {
-        count++;
-    }
-System.Console.Write($"Количество чётных числел в массиве: {count} ");
+// foreach ( int number in myArray)
+//     if (number % 2 == 0)
+//     {
+//         count++;
+//     }
+// System.Console.Write($"Количество чётных числел в массиве: {count} ");
+
+
+// РЕШЕНИЕ С ПОМОЩЬЮ ЦИКЛА FOR
+
+// int count = 0;
+// for (int number = 0; number < 10; number++)
+// {
+//     if (myArray[number] % 2 == 0)
+//     {
+//         count++;
+//     }
+// }
+// System.Console.Write($"Количество чётных числел в массиве: {count} ");
+
+
+// Вывод с помощью функции.
+int countArr = CountDivBy7(myArray);
+System.Console.Write($"Количетсво чётных чисел в массиве: {countArr} ");
